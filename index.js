b4319792971235da5486d13ee875d74fdaf9a8eb
ojeanfeli_pe <jean.felipe.moreira12@gmail.com>
2024-11-12 11:48:06 -0300

    Por fim adicionei um evento de clique nas duvidas, utilizando JavaScript

---
+++ index.js
@@ -0,0 +1,55 @@
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function(duvida){
+    duvida.addEventListener('click', function(){
+        duvida.classList.toggle('ativa')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
